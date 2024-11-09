daa8233ce42eefa9f82ec6843bf1ca97031327b2
[Fernando Chipana] <j.fernando.chipana@gmail.com>
2024-11-09 14:09:45 -0300

    Agregamos las clases restantes

---
+++ MVCBasico/Models/Computadora.cs
@@ -0,0 +1,6 @@
+﻿namespace MVCBasico.Models
+{
+    public class Computadora
+    {
+    }
+}
---
+++ MVCBasico/Models/MediosDePago.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCBasico.Models
+{
+    public enum MediosDePago
+    {
+        TARJETA_CREDITO,
+        TARJETA_DEBITO,
+        MERCADOPAGO
+    }
+}
